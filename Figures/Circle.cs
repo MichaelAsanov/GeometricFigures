445da8b7c02c3970985ca83e5d50a4c2d35efa79
Mickael <MA>
2020-01-14 20:46:04 +0300

    Add some comments

--- Figures/Circle.cs
+++ Figures/Circle.cs
@@ -12,6 +12,11 @@ namespace Figures
         /// </summary>
         public double Radius { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Circle"/> class.
+        /// </summary>
+        /// <param name="radius">Радиус</param>
+        /// <exception cref="ArgumentException"></exception>
         public Circle(double radius)
         {
             if (radius < 0)
@@ -22,6 +27,9 @@ namespace Figures
             Radius = radius;
         }
 
+        /// <summary>
+        /// <inheritdoc cref="Figure"/>
+        /// </summary>
         public override double Area => Math.PI * Radius * Radius;
     }
 }
