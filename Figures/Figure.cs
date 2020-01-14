024ea2f75aa0ea53b16cd8540d9b25ffa5379d46
Mickael <MA>
2020-01-14 19:54:42 +0300

    Add comments for Figure class

--- Figures/Figure.cs
+++ Figures/Figure.cs
@@ -1,8 +1,13 @@
 ﻿namespace Figures
 {
-    
+    /// <summary>
+    /// Класс фигуры
+    /// </summary>
     public abstract class Figure
     {
+        /// <summary>
+        /// Площадь фигуры
+        /// </summary>
         public abstract double Area { get; set; }
     }
 }
